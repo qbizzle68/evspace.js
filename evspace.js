9eb0208dbe494210f1313e9181c917a10af61e49
qbarnes <quinton.barnes68@gmail.com>
2023-03-26 01:47:54 -0500

    created angles and axis types

--- evspace.js
+++ evspace.js
@@ -1,2 +1,4 @@
 export {default as Vector} from './src/vector.js';
 export {default as Matrix} from './src/matrix.js';
+export { Angles, Axis, XYZ, XZY, YXZ, YZX, ZXY, ZYX,
+    XYX, XZX, YXY, YZY, ZXZ, ZYZ } from './src/axis.js';
